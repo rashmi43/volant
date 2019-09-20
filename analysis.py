7ec9d1b67af6cbca93aa481fe14024d72ceb878b
Rashmi Khanna <rashmi.khanna@in.ibm.com>
2019-09-20 20:11:16 +0530

    QUARTILE EXPLAINABILITY

--- analysis.py
+++ analysis.py
@@ -17,33 +17,42 @@ class Analysis:
                     print('**********' + column.upper() + '**********')
                     print('Quartile')
                     print('----------')
-                    print(data[column].quantile([.25, .5, .75]))
+                    print(data[column].quantile([0, .25, .5, .75, 1]))
                     print('Minimum  --->  ', data[column].min())
-                    print('The maximum value of all points in the dataset.')
                     print('Maximum  --->  ', data[column].max())
-                    print('The maximum value of all points in the dataset.')
                     print('Mean     --->  ', data[column].mean())
-                    print('The mean is derived by summing all the data points and dividing by the number of points.')
+
+                    q1_series = data[column].quantile([.25])
+                    # q2 = data[column].quantile([.5])
+
+                    for i, v in q1_series.items():
+                        q1 = v
+
+                    q3_series = data[column].quantile([.75])
+                    for i,v in q3_series.items():
+                        q3 = v
+
+                    iqr = q3 - q1
+                    lower_whisker = q1 - 1.5 * iqr
+                    upper_whisker = q3 + 1.5 * iqr
+                    print('Inter Quartile Range ---> ', iqr, lower_whisker, upper_whisker)
+                    print('Lower Whisker ---> ', lower_whisker)
+                    print('Upper Whisker ---> ', upper_whisker)
+                    outliers = 0
+                    for i in data[column]:
+                        if i < lower_whisker or i > upper_whisker:
+                            outliers = outliers + 1
+
+                    print('Outliers Detected ---> ', outliers)
+
+
+
                     print('Skewness --->', data[column].skew(axis=0, skipna=True))
                     print('**********' + column.upper() + '**********')
-                    print('***************************************')
+
                     print('Explainability for the data')
                     print('To find the quartile of the data, lets sort it first')
-                    print("""Formula for quartile: 
-                        Order the data from least to greatest.
-                        Find the median of the data set and divide the data set into halves.
-                        Find the median of the two halves.""")
-                    sorted_column = data[column].sort_values()
-                    print('sorted column is:', sorted_column)
-                    column_length = data[column].count()
-                    print('Column length is: ', column_length)
-                    median_column = int(column_length/2)
-                    print('Median length is: ', median_column)
 
-                    quartile1 = data[column].iloc[0:median_column]
-                    print('quartile1 is:', quartile1, quartile1.count())
-                    quartile2 = data[column].iloc[median_column:]
-                    print('quartile2 is:', quartile2, quartile2.count())
                     print("""Quartiles in statistics are values that divide your data into quarters.
                         They divide your data into four segments according to where the numbers fall on the number line. 
                         The four quarters that divide a data set into quartiles are:
